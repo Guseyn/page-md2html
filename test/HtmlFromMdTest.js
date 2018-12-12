8d57634115ca8d07f0f401c04162f31e1f16a9d3
gismayilov <guseynism@gmail.com>
2018-12-12 19:28:56 +0300

    change base of lib

--- test/HtmlFromMdTest.js
+++ test/HtmlFromMdTest.js
@@ -5,5 +5,5 @@ const { StrictEqualAssertion } = require('@cuties/assert');
 
 new StrictEqualAssertion(
   new HtmlFromMd('Hello.\n\n* This is markdown.\n* It is fun\n* Love it or leave it.'),
-  '<p>Hello.</p>\n\n<ul><li>This is markdown.</li><li>It is fun</li><li>Love it or leave it.</li></ul>'
+  '<p>Hello.</p>\n<ul>\n<li>This is markdown.</li>\n<li>It is fun</li>\n<li>Love it or leave it.</li>\n</ul>'
 ).call();
